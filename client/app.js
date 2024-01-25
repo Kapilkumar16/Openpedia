3822da6f0b2ef64e1a466e4cf7931f02133820f9
Kapilkumar16 <kapil9559kumar@gmail.com>
2024-01-25 22:45:04 +0530

    fix some bug

--- client/app.js
+++ client/app.js
@@ -4,3 +4,22 @@ let links = document.querySelector('.links');
 hamburger.addEventListener('click', () => {
     links.classList.toggle('toggle-links');
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    var backToTopBtn = document.getElementById("backToTopBtn");
+  
+    window.onscroll = function () {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopBtn.style.display = "block";
+      } else {
+        backToTopBtn.style.display = "none";
+      }
+    };
+  
+    function scrollToTop() {
+        console.log("Button clicked!");
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
+  });
+  
